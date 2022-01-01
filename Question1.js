21f6bcd691a25210d83c1bfd7a4af33112d7548f
panditshubhamcse <93490954+panditshubhamcse@users.noreply.github.com>
2022-01-02 02:37:13 +0530

    Add files via upload
    Commit

---
+++ Question1.js
@@ -0,0 +1,12 @@
+//Q1. Write a program to check whether the number is greater than 10 or not.
+
+
+var number= 1;
+
+if(number > 10)
+{
+    console.log("Number is greater")
+}
+else{
+    console.log("number is small")
+}
---
+++ Question2.js
@@ -0,0 +1,19 @@
+//Q2. Write a program to have 3 variables (all three are number) and output the highest/greatest/maximum of all three.
+
+
+
+var s = 10;
+var h = 20;
+var u = 8;
+
+if( s > h && s > u)
+{
+    console.log(" s is greatest");
+}
+else if ( h > s && h > u)
+{
+    console.log(" h is greatest");
+}
+else{
+    console.log(" u is greatest")
+}
